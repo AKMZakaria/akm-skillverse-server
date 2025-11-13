ec2cd20aaa58afa268eab2215e636251ca5852da
AKMZakaria <akmzakaria488@gmail.com>
2025-11-13 18:33:37 +0600

    cmt added 3

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run() {
       res.send(result)
     })
 
+    // get course by id
     app.get('/courses/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
