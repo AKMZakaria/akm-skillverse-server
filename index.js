4dbaa920188a1134f77ad70356803058af66a02a
AKMZakaria <akmzakaria488@gmail.com>
2025-11-13 18:34:43 +0600

    cmt added 5

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ async function run() {
       res.send(result)
     })
 
+    // update course by id
     app.patch('/myaddedcourses/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
